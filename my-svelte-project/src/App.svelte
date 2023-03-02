263b682a8f1f78f61dfd080e4cabc4d0e887f6e1
leahteich <leahteich@gmail.com>
2023-03-02 12:42:35 -0500

    changes to app.svelte

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -1,22 +1,21 @@
 <script>
   // import Counter from './lib/Counter.svelte'
   import Selector from './lib/Selector.svelte'
+  import Event from './lib/Event.svelte'
 </script>
 
 
 <main>
-  <!-- Make component for toolbar -->
-  <div> toolbar to place event options here 
-    <ul> 
-      <li> [Date] [Time Range]
-      <li> Location: [dropdown]
-      <li> Notes: [text box]
-      <li> Delete 
-    </ul>
-  </div>
-
+  <div id="sticky"> <Event /></div>
   <Selector />
+  
 </main>
 
 <style>
+#sticky {
+  position: sticky!important;
+  top: 1rem;
+  z-index: 3
+}
+
 </style>
