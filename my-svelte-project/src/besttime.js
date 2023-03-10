f4cd0ed63e7568967b193b6528d6f7ac679e76ab
leahteich <leahteich@gmail.com>
2023-03-10 13:37:48 -0500

    fixes

--- my-svelte-project/src/besttime.js
+++ my-svelte-project/src/besttime.js
@@ -6,45 +6,45 @@ import moment from 'moment'
 export function getBest(users) {
 
   let alltimes = {}
-    for (let i=0; i < users.length; i++) {
-      for (let j=0; j<users[i].times.length; j++) {
-        let day = users[i].times[j].split("_")[1]
-        if (!(day in alltimes)) 
-          alltimes[day] = [users[i].times[j].split("_")[0]]
-        else 
-          alltimes[day].push(users[i].times[j].split("_")[0])
-      }
-    }
-    
-    // Availibility Concept: Time blocks  
-    let newtimeslots = {}
-
-    for (let i=0; i < Object.keys(alltimes).length; i++) 
-      newtimeslots[Object.keys(alltimes)[i]] = []
-    
-    for (const [key, value] of Object.entries(alltimes)) {
-      for (let i=0; i < value.length; i++) {
-        let start = moment(value[i].split("-")[0], "hh:mm A")
-        let end = moment(value[i].split("-")[1],"hh:mm A")
-        let newtime = start;
-        while (!(newtime.isSame(end))) {
-          newtimeslots[key].push(newtime.format("hh:mm A"))
-          newtime = start.add(30, 'minutes');
-        }
-      }         
-    }
-
-  let allBest = {}
-
-  // Availibility Concept: Calculating overlap  
-  for (const [key, value] of Object.entries(newtimeslots)) {
-    for (let i=0; i<value.length; i++) {
-      const counts = {}
-      for (const num of value) {
-        counts[num] = counts[num] ? counts[num] + 1 : 1;
-      }
-      allBest[key] = counts
+  for (let i=0; i < users.length; i++) {
+    for (let j=0; j<users[i].times.length; j++) {
+      let day = users[i].times[j].split("_")[1]
+      if (!(day in alltimes)) 
+        alltimes[day] = [users[i].times[j].split("_")[0]]
+      else 
+        alltimes[day].push(users[i].times[j].split("_")[0])
     }
   }
-  return [allBest, newtimeslots]
+
+// Availibility Concept: Time blocks  
+let newtimeslots = {}
+
+for (let i=0; i < Object.keys(alltimes).length; i++) 
+  newtimeslots[Object.keys(alltimes)[i]] = []
+
+for (const [key, value] of Object.entries(alltimes)) {
+  for (let i=0; i < value.length; i++) {
+    let start = moment(value[i].split("-")[0], "hh:mm A")
+    let end = moment(value[i].split("-")[1],"hh:mm A")
+    let newtime = start;
+    while (!(newtime.isSame(end))) {
+      newtimeslots[key].push(newtime.format("hh:mm A"))
+      newtime = start.add(30, 'minutes');
+    }
+  }         
+}
+
+let allBest = {}
+
+// Availibility Concept: Calculating overlap  
+for (const [key, value] of Object.entries(newtimeslots)) {
+  for (let i=0; i<value.length; i++) {
+    const counts = {}
+    for (const num of value) {
+      counts[num] = counts[num] ? counts[num] + 1 : 1;
+    }
+    allBest[key] = counts
+  }
+}
+  return allBest
 }
--- my-svelte-project/src/pages/Selection.svelte
+++ my-svelte-project/src/pages/Selection.svelte
@@ -42,59 +42,21 @@
         $besttimes = []
         const sendtoDB = await db.post(newUser);
         await updateUsers();
+        
 
-        let alltimes = {}
-        for (let i=0; i < users.length; i++) {
-          for (let j=0; j<users[i].times.length; j++) {
-            let day = users[i].times[j].split("_")[1]
-            if (!(day in alltimes)) 
-              alltimes[day] = [users[i].times[j].split("_")[0]]
-            else 
-              alltimes[day].push(users[i].times[j].split("_")[0])
-          }
-        }
-    
-      // Availibility Concept: Time blocks  
-      let newtimeslots = {}
+        let allBest = getBest(users)
 
-      for (let i=0; i < Object.keys(alltimes).length; i++) 
-        newtimeslots[Object.keys(alltimes)[i]] = []
-      
-      for (const [key, value] of Object.entries(alltimes)) {
-        for (let i=0; i < value.length; i++) {
-          let start = moment(value[i].split("-")[0], "hh:mm A")
-          let end = moment(value[i].split("-")[1],"hh:mm A")
-          let newtime = start;
-          while (!(newtime.isSame(end))) {
-            newtimeslots[key].push(newtime.format("hh:mm A"))
-            newtime = start.add(30, 'minutes');
+        let besttime;
+        let bestday;
+        let bestcount = 0;
+        for (const [day, times] of Object.entries(allBest)) {
+          for (const [time, instances] of Object.entries(times)) {
+            if (instances >= bestcount) {
+              besttime = time
+              bestcount = instances
+              bestday = day
+            }
           }
-        }         
-      }
-
-      let allBest = {}
-
-      // Availibility Concept: Calculating overlap  
-      for (const [key, value] of Object.entries(newtimeslots)) {
-        for (let i=0; i<value.length; i++) {
-          const counts = {}
-          for (const num of value) {
-            counts[num] = counts[num] ? counts[num] + 1 : 1;
-          }
-          allBest[key] = counts
-        }
-      }
-      let besttime;
-      let bestday;
-      let bestcount = 0;
-      for (const [day, times] of Object.entries(allBest)) {
-        for (const [time, instances] of Object.entries(times)) {
-          if (instances >= bestcount) {
-            besttime = time
-            bestcount = instances
-            bestday = day
-          }
-        }
       }
       let origbesttime = besttime
       let endbesttime = moment(besttime, "hh:mm A").add(30, 'minutes').format("hh:mm A")
